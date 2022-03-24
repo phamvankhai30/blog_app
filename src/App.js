045567f266277cc7ee82b0ee53d9e5537b3abc29
Khai Pham <pvkhai30@gmail.com>
2022-03-24 23:14:56 +0700

    commit 24/03/22

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Login from "./pages/login/Login";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Setting from "./pages/setting/Setting";
-import Example1 from "./Example/example1/Example1";
-import Example2 from "./Example/example2/Example2";
-import HomeEx3 from "./Example/example3/HomeEx3";
-import ProductEx3 from "./Example/example3/ProductEx3";
-import BlogEx3 from "./Example/example3/BlogEx3";
-import Example3 from "./Example/example3/Example3";
-import Example4 from "./Example/example4/Example4";
-import Example5 from "./Example/example5/Example5";
-import Example6 from "./Example/example6/Example6";
-import Example7 from "./Example/example7/Example7";
+// import Example1 from "./Example/example1/Example1";
+// import Example2 from "./Example/example2/Example2";
+// import HomeEx3 from "./Example/example3/HomeEx3";
+// import ProductEx3 from "./Example/example3/ProductEx3";
+// import BlogEx3 from "./Example/example3/BlogEx3";
+// import Example3 from "./Example/example3/Example3";
+// import Example4 from "./Example/example4/Example4";
+// import Example5 from "./Example/example5/Example5";
+// import Example6 from "./Example/example6/Example6";
+// import Example7 from "./Example/example7/Example7";
 
 function App() {
   const currentUser = true;
@@ -25,11 +25,11 @@ function App() {
       <TopBar />
 
       {/* example3 */}
-      <Example3 />
+      {/* <Example3 /> */}
 
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/posts" element={<HomePage />} />
+        {/* <Route exact path="/posts" element={<HomePage />} /> */}
         {/* <Route
           path="/register"
           element={currentUser ? <HomePage /> : <Register />}
@@ -47,7 +47,7 @@ function App() {
         <Route path="/setting" element={<Setting />} />
 
         {/*start Example */}
-        <Route path="/ex1" element={<Example1 />} />
+        {/* <Route path="/ex1" element={<Example1 />} />
         <Route path="/ex2" element={<Example2 />} />
         <Route path="/ex31" element={<HomeEx3 />} />
         <Route path="/ex32" element={<ProductEx3 />} />
@@ -55,7 +55,7 @@ function App() {
         <Route path="/ex4" element={<Example4 />} />
         <Route path="/ex5" element={<Example5 />} />
         <Route path="/ex6" element={<Example6 />} />
-        <Route path="/ex7" element={<Example7 />} />
+        <Route path="/ex7" element={<Example7 />} /> */}
         {/* end Example */}
       </Routes>
     </>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css";
+import axios from "axios";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  //const { search } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts");
+      console.log(res);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <>
       <Header />
