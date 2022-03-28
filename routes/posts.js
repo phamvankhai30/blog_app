24d8a93b5257c884546d1971fbac6b853b0be8e6
Nhật Khánh <khanhpham2708@gmail.com>
2022-03-28 21:59:20 +0700

    update search for input

--- routes/posts.js
+++ routes/posts.js
@@ -1,82 +1,83 @@
-const router = require("express").Router();
-const User = require("../models/User");
-const Post = require("../models/Post");
+const router = require('express').Router()
+const User = require('../models/User')
+const Post = require('../models/Post')
 
 //CREATE POST
-router.post("/", async (req, res) => {
-  const newPost = new Post(req.body);
-  try {
-    const savedPost = await newPost.save();
-    res.status(200).json(savedPost);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.post('/', async (req, res) => {
+	const newPost = new Post(req.body)
+	try {
+		const savedPost = await newPost.save()
+		res.status(200).json(savedPost)
+	} catch (err) {
+		res.status(500).json(err)
+	}
+})
 
 //UPDATE POST
-router.put("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.username === req.body.username) {
-      try {
-        const updatedPost = await Post.findByIdAndUpdate(
-          req.params.id,
-          {
-            $set: req.body,
-          },
-          { new: true }
-        );
-        res.status(200).json(updatedPost);
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(401).json("You can update only your post!");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.put('/:id', async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id)
+		if (post.username === req.body.username) {
+			try {
+				const updatedPost = await Post.findByIdAndUpdate(
+					req.params.id,
+					{
+						$set: req.body,
+					},
+					{ new: true }
+				)
+				res.status(200).json(updatedPost)
+			} catch (err) {
+				res.status(500).json(err)
+			}
+		} else {
+			res.status(401).json('You can update only your post!')
+		}
+	} catch (err) {
+		res.status(500).json(err)
+	}
+})
 
 //DELETE POST
-router.delete("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.username === req.body.username) {
-      try {
-        await post.delete();
-        res.status(200).json("Post has been deleted...");
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(401).json("You can delete only your post!");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.delete('/:id', async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id)
+		if (post.username === req.body.username) {
+			try {
+				await post.delete()
+				res.status(200).json('Post has been deleted...')
+			} catch (err) {
+				res.status(500).json(err)
+			}
+		} else {
+			res.status(401).json('You can delete only your post!')
+		}
+	} catch (err) {
+		res.status(500).json(err)
+	}
+})
 
 //GET POST
-router.get("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    res.status(200).json(post);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get('/:id', async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id)
+		res.status(200).json(post)
+	} catch (err) {
+		res.status(500).json(err)
+	}
+})
 
 //GET ALL POSTS
-router.get("/", async (req, res) => {
-  const username = req.query.user;
-  const catName = req.query.cat;
-  const title = req.query.title;
-  try {
-    let posts;
-    if (username) {
-      posts = await Post.find({ username });
-    } else if (catName) {
+router.get('/', async (req, res) => {
+	const username = req.query.user
+	const catName = req.query.cat
+	const title = req.query.title
+	const search = req.query.search
+	try {
+		let posts
+		if (username) {
+			posts = await Post.find({ username })
+		} else if (catName) {
 			posts = await Post.find({
 				categories: {
 					$in: [catName],
@@ -84,13 +85,21 @@ router.get("/", async (req, res) => {
 			})
 		} else if (title) {
 			posts = await Post.find({ title: new RegExp(title) })
+		} else if (search) {
+			//Nếu @ ở vị trí 0 thì tìm theo user, ngược lại tìm theo title
+			if (search.charAt(0) === '@') {
+				const username = search.substring(1)
+				posts = await Post.find({ username })
+			} else {
+				posts = await Post.find({ title: new RegExp(search) })
+			}
 		} else {
 			posts = await Post.find()
 		}
-    res.status(200).json(posts);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+		res.status(200).json(posts)
+	} catch (err) {
+		res.status(500).json(err)
+	}
+})
 
-module.exports = router;
+module.exports = router
