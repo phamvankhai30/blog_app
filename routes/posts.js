699dc14e7810221384180fe87e5d3f4f9264536a
Nhật Khánh <khanhpham2708@gmail.com>
2022-03-28 19:48:52 +0700

    add find by title of posts

--- routes/posts.js
+++ routes/posts.js
@@ -71,19 +71,22 @@ router.get("/:id", async (req, res) => {
 router.get("/", async (req, res) => {
   const username = req.query.user;
   const catName = req.query.cat;
+  const title = req.query.title;
   try {
     let posts;
     if (username) {
       posts = await Post.find({ username });
     } else if (catName) {
-      posts = await Post.find({
-        categories: {
-          $in: [catName],
-        },
-      });
-    } else {
-      posts = await Post.find();
-    }
+			posts = await Post.find({
+				categories: {
+					$in: [catName],
+				},
+			})
+		} else if (title) {
+			posts = await Post.find({ title: new RegExp(title) })
+		} else {
+			posts = await Post.find()
+		}
     res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
